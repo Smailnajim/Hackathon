8a7eb41310ecba6eb811f6933e4b81b2a578c522
smailnajim <smailnajim>
2025-03-18 18:02:54 +0100

    auth lgin, logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -28,5 +29,23 @@ class AuthController extends Controller
         return response()->json(['token' => $token, 201]);
     }
 
-    
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|min5',
+        ]);
+
+        $user = User::find('email', $request->email); 
+        if (!$user || $user->password !== $request->password){
+            return response()->json(['state' => 'this email or password not corecte'], 404);
+        }
+
+        $token = JWTAuth::fromUser($user);
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function logout(){
+        Auth::logout();
+        return response(['state' => 'logout By seccessflly']);
+    }
 }
